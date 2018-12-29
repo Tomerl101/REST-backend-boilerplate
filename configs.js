89bdca027d357e13ed6c6e937e8e7f7af4c2555e
Tomer <tomerl101@gmail.com>
2018-12-29 13:36:31 +0200

    configs use production env variables

--- configs.js
+++ configs.js
@@ -1,2 +1,5 @@
 const { parsed: configs } = require('dotenv').config();
+configs.DB_URL = configs.DB_URL || process.env.DB_URL;
+configs.DB_USER = configs.DB_USER || process.env.DB_USER;
+configs.DB_PASS = configs.DB_PASS || process.env.DB_PASS;
 module.exports = configs;
